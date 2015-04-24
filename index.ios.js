a40fd0755d231dc5e95cad4eacd394d187973a87
Gregg Meluski <gmeluski@hotmail.com>
2015-04-24 17:47:22 -0400

    initial commit

---
+++ index.ios.js
@@ -0,0 +1,48 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS
+} = React;
+
+var githubNotetaker = React.createClass({
+  render: function() {
+    var name = 'Gregg';
+    return (
+      <NavigatorIOS
+        initialRoute={{
+          title: 'github Notetaker',
+          component: Main
+        }} />
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('githubNotetaker', () => githubNotetaker);
